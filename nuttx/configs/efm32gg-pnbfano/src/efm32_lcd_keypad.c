5d20cb787508edcdf70d55beb5430c0c7cfb3009
pnb <pn_bouteville@yahoo.fr>
2014-11-24 23:16:56 +0100

    key board on LCD seems to works with poll function

--- nuttx/configs/efm32gg-pnbfano/src/efm32_lcd_keypad.c
+++ nuttx/configs/efm32gg-pnbfano/src/efm32_lcd_keypad.c
@@ -45,6 +45,7 @@
 #include <arch/board/board.h>
 
 #include <string.h>
+#include <poll.h>
 #include <errno.h>
 #include <nuttx/kmalloc.h>
 #include <nuttx/wqueue.h>
@@ -57,9 +58,9 @@
 
 #include <efm32_lcd_keypad.h>
 
-//#define EFM32_LCD_KBD_LOG(...)
+#define EFM32_LCD_KBD_LOG(...)
 //#define EFM32_LCD_KBD_LOG(...) lldbg(__VA_ARGS__)
-#define EFM32_LCD_KBD_LOG(...) syslog(LOG_NOTICE,__VA_ARGS__)
+//#define EFM32_LCD_KBD_LOG(...) syslog(LOG_NOTICE,__VA_ARGS__)
 
 #ifndef CONFIG_EFM32_LCD_KBD_BUFSIZE
 #  define CONFIG_EFM32_LCD_KBD_BUFSIZE 64
@@ -75,33 +76,6 @@
 
 typedef FAR struct file file_t;
 
-static int efm32_lcd_kbd_open(file_t * filep);
-static int efm32_lcd_kbd_close(file_t * filep);
-static ssize_t efm32_lcd_kbd_read(file_t * filep, FAR char *buffer, size_t buflen);
-#ifndef CONFIG_DISABLE_POLL
-/* TODO */
-//static int efm32_lcd_kbd_poll(file_t * filep, FAR struct pollfd *fds, bool setup);
-#endif
-
-static const struct file_operations keypad_ops =
-{
-    efm32_lcd_kbd_open,  /* open */
-    efm32_lcd_kbd_close, /* close */
-    efm32_lcd_kbd_read,  /* read */
-    NULL,                 /* write */
-    NULL,                 /* seek */
-    NULL,                 /* ioctl */
-#ifndef CONFIG_DISABLE_POLL
-    NULL,                 /* TODO: efm32_lcd_kbd_poll */ /* poll */
-#endif
-};
-
-
-/****************************************************************************
- * Name: efm32_lcd_kbd_t
- * Description:
- *  variable of keypad
- ****************************************************************************/
 typedef struct 
 {
     sem_t   mutex;
@@ -110,15 +84,73 @@ typedef struct
     int     wr_idx;
     uint8_t kbdbuffer[CONFIG_EFM32_LCD_KBD_BUFSIZE];
 
+    sem_t   *poll_sem;
     struct work_s work;
 }efm32_lcd_kbd_t;
 
-/****************************************************************************
- * Name: efm32_lcd_kbd
- * Description:
- *  variable of keypad instance.
- ****************************************************************************/
-efm32_lcd_kbd_t* efm32_lcd_kbd = NULL;
+struct efm32_lcd_kbd_stream 
+{
+    struct lib_outstream_s  stream;
+    efm32_lcd_kbd_t         *dev;
+};
+
+
+static int efm32_lcd_kbd_open(file_t * filep);
+static int efm32_lcd_kbd_close(file_t * filep);
+static ssize_t efm32_lcd_kbd_read(file_t * filep, FAR char *buffer, size_t buflen);
+#ifndef CONFIG_DISABLE_POLL
+static int efm32_lcd_kbd_poll(file_t * filep, FAR struct pollfd *fds, bool setup);
+#endif
+
+static const struct file_operations keypad_ops =
+{
+    efm32_lcd_kbd_open,  /* open    */
+    efm32_lcd_kbd_close, /* close   */
+    efm32_lcd_kbd_read,  /* read    */
+    NULL,                /* write   */
+    NULL,                /* seek    */
+    NULL,                /* ioctl   */
+#ifndef CONFIG_DISABLE_POLL
+    efm32_lcd_kbd_poll,  /* poll    */
+#endif
+};
+
+/************************************************************************************
+ * Name: efm32_lcd_kbd_takesem
+ ************************************************************************************/
+
+static int efm32_lcd_kbd_takesem(FAR sem_t *sem, bool errout)
+{
+  /* Loop, ignoring interrupts, until we have successfully acquired the semaphore */
+
+  while (sem_wait(sem) != OK)
+    {
+      /* The only case that an error should occur here is if the wait was awakened
+       * by a signal.
+       */
+
+      ASSERT(get_errno() == EINTR);
+
+      /* When the signal is received, should we errout? Or should we just continue
+       * waiting until we have the semaphore?
+       */
+
+      if (errout)
+        {
+          return -EINTR;
+        }
+    }
+
+  return OK;
+}
+
+/************************************************************************************
+ * Name: uart_givesem
+ ************************************************************************************/
+
+#define efm32_lcd_kbd_givesem(sem) (void)sem_post(sem)
+
+
 
 /****************************************************************************
  * Name: key_mapping
@@ -139,45 +171,66 @@ static const int key_mapping[GPIO_LCD_PORT_BUS_WIDTH] =
 static void efm32_lcd_kbd_worker(FAR void *arg);
 
 /****************************************************************************
- * Name: efm32_lcd_kbd_open
+ * Name: efm32_lcd_kbd_level
  ****************************************************************************/
-static void efm32_lcd_kbd_putc(FAR struct lib_outstream_s *this, int ch)
-{
-    if ( efm32_lcd_kbd == NULL )
-    {
-        EFM32_LCD_KBD_LOG("driver not initialized!\n");
-        return; 
-    }
 
-    int level = efm32_lcd_kbd->wr_idx - efm32_lcd_kbd->rd_idx;
+int efm32_lcd_kbd_level(FAR efm32_lcd_kbd_t *dev)
+{
+    int level = dev->wr_idx - dev->rd_idx;
 
     if ( level < 0 )
         level += CONFIG_EFM32_LCD_KBD_BUFSIZE;
 
-    if (level >= CONFIG_EFM32_LCD_KBD_BUFSIZE)
-    {
-        EFM32_LCD_KBD_LOG("Buffer overflow\n");
-        return; 
-    }
-
-    efm32_lcd_kbd->kbdbuffer[efm32_lcd_kbd->wr_idx++] = ch;
-    if ( efm32_lcd_kbd->wr_idx >= CONFIG_EFM32_LCD_KBD_BUFSIZE )
-        efm32_lcd_kbd->wr_idx = 0;
-
-    sem_post(&efm32_lcd_kbd->rd_sem);
-
-    this->nput++;
+    return level;
 }
 
 /****************************************************************************
  * Name: efm32_lcd_kbd_open
  ****************************************************************************/
-static void efm32_lcd_kbd_set_next_poll(void)
+
+static void efm32_lcd_kbd_putc(FAR struct lib_outstream_s *this, int ch)
+{
+    struct efm32_lcd_kbd_stream *p = (struct efm32_lcd_kbd_stream*)this;
+    efm32_lcd_kbd_t *dev = p->dev;
+
+    if ( dev == NULL )
+    {
+        EFM32_LCD_KBD_LOG("driver not initialized!\n");
+        return; 
+    }
+
+    sem_wait(&dev->mutex);
+    if ( efm32_lcd_kbd_level(dev) < CONFIG_EFM32_LCD_KBD_BUFSIZE )
+    {
+        dev->kbdbuffer[dev->wr_idx++] = ch;
+        if ( dev->wr_idx >= CONFIG_EFM32_LCD_KBD_BUFSIZE )
+            dev->wr_idx = 0;
+
+        sem_post(&dev->rd_sem);
+        this->nput++;
+
+        /* add event to waiting semaphore */
+        if ( dev->poll_sem )
+        {
+            sem_post( dev->poll_sem );
+        }
+    }
+    else
+    {
+        EFM32_LCD_KBD_LOG("Buffer overflow\n");
+    }
+    sem_post(&dev->mutex);
+}
+
+/****************************************************************************
+ * Name: efm32_lcd_kbd_open
+ ****************************************************************************/
+static void efm32_lcd_kbd_set_next_poll(efm32_lcd_kbd_t* dev)
 {
     if ( work_queue(HPWORK, 
-                    &(efm32_lcd_kbd->work), 
+                    &(dev->work), 
                     efm32_lcd_kbd_worker,
-                    &efm32_lcd_kbd, 
+                    dev, 
                     MSEC2TICK(CONFIG_LCD_KBD_POLL_MS)
                     ) != OK )
     {
@@ -194,6 +247,7 @@ static void efm32_lcd_kbd_worker(FAR void *arg)
     int res ;
     int i;
     static int last_res = 0;
+    efm32_lcd_kbd_t *dev = (efm32_lcd_kbd_t*)arg;
 
     /* TODO: use arg
      * efm32_lcd_kbd_t *p = (efm32_lcd_kbd_t*)arg, 
@@ -215,9 +269,9 @@ static void efm32_lcd_kbd_worker(FAR void *arg)
                  && ( res & mask ) != ( last_res & mask ) 
                )
             {
-                struct lib_outstream_s stream = {
-                    .put  = efm32_lcd_kbd_putc,
-                    .nput = 0
+                struct efm32_lcd_kbd_stream stream = {
+                    .stream = { .put  = efm32_lcd_kbd_putc, .nput = 0 },
+                    .dev    = dev
                 };
 
                 if ( res & mask )
@@ -225,12 +279,14 @@ static void efm32_lcd_kbd_worker(FAR void *arg)
                     if ( EFM32_LCD_KEY_IS_SPECIAL(key_mapping[i]) )
                     {
                         kbd_specpress(EFM32_LCD_KEY_SPECIAL(key_mapping[i]), 
-                                      &stream
+                                      (struct lib_outstream_s*)&stream
                                      );
                     }
                     else
                     {
-                        kbd_press(key_mapping[i], &stream);
+                        kbd_press(key_mapping[i], 
+                                  (struct lib_outstream_s*)&stream
+                                 );
                     }
                 }
                 else
@@ -238,12 +294,14 @@ static void efm32_lcd_kbd_worker(FAR void *arg)
                     if ( EFM32_LCD_KEY_IS_SPECIAL(key_mapping[i]) )
                     {
                         kbd_specrel(EFM32_LCD_KEY_SPECIAL(key_mapping[i]), 
-                                    &stream
+                                    (struct lib_outstream_s*)&stream
                                    );
                     }
                     else
                     {
-                        kbd_release(key_mapping[i], &stream);
+                        kbd_release(key_mapping[i], 
+                                    (struct lib_outstream_s*)&stream
+                                   );
                     }
                 }
             }
@@ -251,7 +309,7 @@ static void efm32_lcd_kbd_worker(FAR void *arg)
     }
     last_res = res;
 
-    efm32_lcd_kbd_set_next_poll();
+    efm32_lcd_kbd_set_next_poll(dev);
 
     return;
 }
@@ -271,30 +329,32 @@ static void efm32_lcd_kbd_worker(FAR void *arg)
  ****************************************************************************/
 int keypad_kbdinit( void )
 {
+    static efm32_lcd_kbd_t * dev = NULL;
 
     /* can be called only once */
 
-    if ( efm32_lcd_kbd != NULL)
+    if ( dev != NULL)
     {
         EFM32_LCD_KBD_LOG("Already initialized !\n");
         return OK;
     }
 
-    efm32_lcd_kbd = (efm32_lcd_kbd_t*)kmm_malloc(sizeof(efm32_lcd_kbd_t));
-    if ( efm32_lcd_kbd == NULL )
+
+    dev = (efm32_lcd_kbd_t*)kmm_malloc(sizeof(efm32_lcd_kbd_t));
+    if ( dev == NULL )
     {
         EFM32_LCD_KBD_LOG("Cannot allocate it!\n");
         return -ENODEV;
     }
 
-    memset(efm32_lcd_kbd,0,sizeof(*efm32_lcd_kbd));
+    memset(dev,0,sizeof(*dev));
+    //dev->poll_sem = NULL; already done */
+    sem_init(&dev->rd_sem,    1, 0);
+    sem_init(&dev->mutex,     1, 1);
 
-    sem_init(&efm32_lcd_kbd->rd_sem,    1, 0);
-    sem_init(&efm32_lcd_kbd->mutex,     1, 1);
+    efm32_lcd_kbd_set_next_poll(dev);
 
-    efm32_lcd_kbd_set_next_poll();
-
-    register_driver("/dev/keypad", &keypad_ops, 0444, NULL);
+    register_driver("/dev/keypad", &keypad_ops, 0444, dev);
 
     return OK;
 }
@@ -307,7 +367,10 @@ int keypad_kbdinit( void )
 
 static int efm32_lcd_kbd_open(file_t * filep)
 {
-    if ( efm32_lcd_kbd == NULL )
+    FAR struct inode *inode     = filep->f_inode;
+    FAR efm32_lcd_kbd_t *dev    = inode->i_private;
+
+    if ( dev == NULL )
     {
         EFM32_LCD_KBD_LOG("Not initialized!\n");
         return -EINVAL;
@@ -329,6 +392,70 @@ static int efm32_lcd_kbd_close(file_t * filep)
 }
 
 /****************************************************************************
+ * Name: efm32_lcd_kbd_poll
+ ****************************************************************************/
+
+#ifndef CONFIG_DISABLE_POLL
+static int efm32_lcd_kbd_poll(file_t * filep, FAR struct pollfd *fds, bool setup)
+{
+
+    FAR struct inode *inode     = filep->f_inode;
+    FAR efm32_lcd_kbd_t *dev    = inode->i_private;
+
+    int res = 0;
+
+    /* Are we setting up the poll?  Or tearing it down? */
+
+    res = efm32_lcd_kbd_takesem(&dev->mutex, true);
+
+    if (res < 0)
+    {
+        /* A signal received while waiting for access to the poll data
+         * will abort the operation.
+         */
+
+        return res;
+    }
+
+    if (setup)
+    {
+        fds->revents = 0;
+        /* This is a request to set up the poll.  Find an available
+         * slot for the poll structure reference
+         */
+
+        if ( dev->poll_sem != NULL)
+        {
+            res = -EINVAL;
+            goto errout;
+        }
+
+        if ( efm32_lcd_kbd_level(dev) > 0 )
+        {
+            fds->revents |= (fds->events & POLLIN);
+        }
+
+        if ( fds->revents == 0 )
+        {
+            dev->poll_sem = fds->sem ;
+        }
+        else
+        {
+            sem_post(fds->sem);
+            res = 1;
+        }
+    }
+    else if ( dev->poll_sem == fds->sem )
+    {
+        dev->poll_sem = NULL;
+    }
+errout:
+    efm32_lcd_kbd_givesem(&dev->mutex);
+    return res;
+}
+#endif
+
+/****************************************************************************
  * Name: efm32_lcd_kbd_read
  ****************************************************************************/
 
@@ -336,13 +463,16 @@ static ssize_t efm32_lcd_kbd_read(file_t * filep, FAR char *buf, size_t buflen)
 {
     ssize_t size = 0;
 
-    if ( efm32_lcd_kbd == NULL )
+    FAR struct inode *inode     = filep->f_inode;
+    FAR efm32_lcd_kbd_t *dev    = inode->i_private;
+
+    if ( dev == NULL )
     {
         EFM32_LCD_KBD_LOG("Not initialized!\n");
         return -EINVAL;
     }
 
-    sem_wait( &efm32_lcd_kbd->mutex );
+    sem_wait( &dev->mutex );
 
     while (size < buflen)
     {
@@ -350,8 +480,8 @@ static ssize_t efm32_lcd_kbd_read(file_t * filep, FAR char *buf, size_t buflen)
         /* first lock it then only try lock */
 
         if ( size == 0 )
-            sem_wait( &efm32_lcd_kbd->rd_sem);
-        else if ( sem_trywait( &efm32_lcd_kbd->rd_sem) < 0 )
+            sem_wait( &dev->rd_sem);
+        else if ( sem_trywait( &dev->rd_sem) < 0 )
             break;
 
         irqstate_t saved_state;
@@ -359,9 +489,9 @@ static ssize_t efm32_lcd_kbd_read(file_t * filep, FAR char *buf, size_t buflen)
 
         saved_state = irqsave();
 
-        buf[size] = efm32_lcd_kbd->kbdbuffer[efm32_lcd_kbd->rd_idx++];
-        if (efm32_lcd_kbd->rd_idx >= CONFIG_EFM32_LCD_KBD_BUFSIZE )
-            efm32_lcd_kbd->rd_idx=0;
+        buf[size] = dev->kbdbuffer[dev->rd_idx++];
+        if (dev->rd_idx >= CONFIG_EFM32_LCD_KBD_BUFSIZE )
+            dev->rd_idx=0;
 
         irqrestore(saved_state);
 
@@ -370,7 +500,7 @@ static ssize_t efm32_lcd_kbd_read(file_t * filep, FAR char *buf, size_t buflen)
         size++; 
     }
 
-    sem_post( &efm32_lcd_kbd->mutex );
+    sem_post( &dev->mutex );
 
     return size;
 }
